30163b7e9b8b4652f6d562c71d5acf633c952b17
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-05-19 10:54:19 +0300

    Making Swipes on Dots
    Dots now can be swiped but has some bugs

--- Match3/Assets/Scripts/Dot.cs
+++ Match3/Assets/Scripts/Dot.cs
@@ -4,24 +4,48 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour
 {
+    public int column;
+    public int row;
+    public int targetX;
+    public int targetY;
+    private Board board;
+    private GameObject otherDot;
 
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
+    private Vector2 tempPosition;
     public float swipeAngle = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        board= FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        column= targetX;
+        row = targetY;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetX = column;
+        targetY = row ;
+        if(Mathf.Abs(targetX-transform.position.x) > .1)
+        {
+            //Move towards the target
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }else
+        {//Directly set position
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            board.allDots[column, row] = this.gameObject;
+        }
+
     }
     private void OnMouseDown()
     {
-        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);   
         
     }
 
@@ -35,5 +59,33 @@ public class Dot : MonoBehaviour
     {
         swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180/Mathf.PI;
         Debug.Log(swipeAngle);
+        MovePieces();
+    }
+
+    void MovePieces()
+    {
+        if(swipeAngle > -45 && swipeAngle <= 45 && column<board.width)
+        {// right swipe
+            otherDot= board.allDots[column + 1,row];
+            otherDot.GetComponent<Dot>().column -= 1;
+            column += 1;
+        }else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
+        {// up swipe
+            otherDot = board.allDots[column , row+1];
+            otherDot.GetComponent<Dot>().row -= 1;
+            row += 1;
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && column >0)
+        {//left swipe
+            otherDot = board.allDots[column - 1, row];
+            otherDot.GetComponent<Dot>().column += 1;
+            column -= 1;
+        }
+        else if (swipeAngle >= -135 && swipeAngle < -45 && row >0)
+        {// down swipe
+            otherDot = board.allDots[column , row -1];
+            otherDot.GetComponent<Dot>().row += 1;
+            row -= 1;
+        }
     }
 }
